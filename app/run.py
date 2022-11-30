83ca30a4965638f914c5a651b3988cc79d45ff41
Juan Elias Rodriguez <stylorj@gmail.com>
2022-11-29 23:04:22 -0300

    fix video streaming encode with content-type: image/jpeg

--- app/run.py
+++ app/run.py
@@ -1,10 +1,8 @@
 import cv2
 import uvicorn
 from fastapi import FastAPI
-import asyncio
 from inference import inference
-from concurrent.futures import ProcessPoolExecutor
-from functools import partial
+from fastapi.responses import StreamingResponse
 
 
 app = FastAPI()
@@ -15,29 +13,24 @@ def read_root():
     return {"message": "Welcome from the API"}
 
 
-@app.post("/inference")
-async def get_image():
+@app.post("/video_stream")
+async def video_stream():
+    return StreamingResponse(get_image(), media_type="multipart/x-mixed-replace;boundary=frame")
+
+
+def get_image():
     cap = cv2.VideoCapture(0)
-    frame = cap.read()
-    model = "YOLOv5s.onnx"
-    output = inference.inference(model, frame)
-    # asyncio.create_task(generate_remaining_models(model, frame))
-    # TODO: ENCODE OUTPUT IMAGE
-    return output
-
-
-async def generate_remaining_models(models, image):
-    executor = ProcessPoolExecutor()
-    event_loop = asyncio.get_event_loop()
-    await event_loop.run_in_executor(
-        executor, partial(process_image, models, image)
-    )
-
-
-def process_image(models, image):
-    for model in models:
-        output = inference.inference(model, image)
-        return output
+    while True:
+        frame = cap.read()
+        model = "YOLOv5s.onnx"
+        output = inference.inference(model, frame)
+        # asyncio.create_task(generate_remaining_models(model, frame))
+        if output is None:
+            continue
+        (flag, encodedImage) = cv2.imencode(".jpg", output)
+        response = (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+                    bytearray(encodedImage) + b'\r\n')
+        return response
 
 
 if __name__ == "__main__":
