7f1d7b8ded383c92ec44705c38b2dbbfa68fca91
Juan Elias Rodriguez <stylorj@gmail.com>
2023-02-28 20:01:11 -0300

    conteo de frames y borrado de cache al iniciar stream

--- app/main/router/stream_routes.py
+++ app/main/router/stream_routes.py
@@ -1,19 +1,20 @@
 import cv2
 from fastapi import APIRouter, Request
-from ..services import inference
+from ..services import inference, redis_client
 from fastapi.responses import StreamingResponse
 from ..config import Settings
 from fastapi.templating import Jinja2Templates
 import time
 import asyncio
-from ..utils.timer_utils import start_timer
+from ..schemas.timer_schema import Timer
+from ..utils.timer_utils import start_timer, finish_timer
 
 config = Settings()
 router = APIRouter(prefix="/stream", tags=["Video Stream"])
 templates = Jinja2Templates(directory=config.TEMPLATE_PATH)
 
 
-@router.get("/")
+@router.get("/test")
 async def read_root(request: Request):
     """
     Endpoint for test Stream result.
@@ -42,28 +43,36 @@ async def get_image():
 
     :return:
     """
-    timer = {
-        "timer1": True,
-        "timer2": True,
-        "timer_limit_start_save": start_timer(100),
-        "timer_limit_end_save": start_timer(100),
-    }
-    cap = cv2.VideoCapture("M.mp4") #udp://192.168.1.49:8080
+    timer1 = Timer()
+    timer2 = Timer()
+    num_frames = 0
+    redis_client.delete_all_cache()
     model = f"{config.MODEL_PATH}/best.onnx"  # Cambiar al nombre del modelo que quiere probar
     classesfile = config.CLASSES_PATH
     with open(classesfile, 'rt') as f: # Obtener las clases al predecir
         classes = f.read().rstrip('\n').split('\n')
 
     net = cv2.dnn.readNetFromONNX(model)
-    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA) # Seleccionar cuda para inferir en GPU
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)  # Seleccionar cuda para inferir en GPU
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+    cap = cv2.VideoCapture("M.mp4")  # udp://192.168.1.49:8080?overrun_nonfatal
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
     while True:
         ret, frame = cap.read()
+
         # output = await inference.inference(net, frame, classes)
 
         if ret:
+            num_frames += 1
+            if timer2.flag1:
+                timer2.timer_limit_start_save = start_timer(10)
+                timer2.flag1 = False
+            if finish_timer(timer2.timer_limit_start_save):
+                timer2.flag1 = True
+                print(num_frames)  # todo print solo para pruebas
+                num_frames = 0
 
-            task = asyncio.create_task(inference.inference(net, frame, classes, timer))
+            task = asyncio.create_task(inference.inference(net, frame, classes, timer1))
             output = await task
             if output is None:
                 continue
